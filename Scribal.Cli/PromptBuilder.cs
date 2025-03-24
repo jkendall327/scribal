11fcb68d9faf0f203b389f9fcb4a196693da4e91
jackkendall <jkendall3096@gmail.com>
2025-03-24 20:49:24 +0000

    feat: Add PromptBuilder class with initial BuildPromptAsync method

---
+++ Scribal.Cli/PromptBuilder.cs
@@ -0,0 +1,9 @@
+namespace Scribal.Cli;
+
+public class PromptBuilder(IDocumentScanService scanService)
+{
+    public async Task<string> BuildPromptAsync()
+    {
+        return string.Empty;
+    }
+}

b18a6fc797eb65bca5a8cb5aabb343139c81809b
jackkendall <jkendall3096@gmail.com>
2025-03-26 19:04:22 +0000

    refactor: Remove unused Ollama.Chat import from Program.cs

--- Scribal.Cli/Program.cs
+++ Scribal.Cli/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OpenAI;
-using Ollama.Chat;
 using Scribal.Cli;
 
 var builder = Host.CreateApplicationBuilder(args);

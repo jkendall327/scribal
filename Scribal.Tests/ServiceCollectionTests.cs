7076cd2b01aa54745d507e6a5c664c6e2a20e1c6
jackkendall <jkendall3096@gmail.com>
2025-05-04 20:42:34 +0100

    Add test for DI

---
+++ Scribal.Tests/ServiceCollectionTests.cs
@@ -0,0 +1,36 @@
+﻿using System.IO.Abstractions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Time.Testing;
+using NSubstitute;
+
+namespace Scribal.Tests;
+
+public class ServiceCollectionTests
+{
+    [Fact]
+    public void ServiceProviderBuildsCorrectly()
+    {
+        var services = new ServiceCollection();
+
+        var filesystem = Substitute.For<IFileSystem>();
+        
+        var config = new ConfigurationBuilder().AddInMemoryCollection([
+            new("OpenAI:ApiKey", "Foo"),
+        ]).Build();
+        
+        services.AddSingleton<IConfiguration>(config);
+        services.AddSingleton(typeof(ILogger<>), typeof(NullLogger<>));
+        
+        services.AddScribal(filesystem, new FakeTimeProvider());
+        services.AddScribalAi(config);
+        
+        var provider = services.BuildServiceProvider(new ServiceProviderOptions
+        {
+            ValidateScopes = true,
+            ValidateOnBuild = true,
+        });
+    }
+}
--- Scribal.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Scribal.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
